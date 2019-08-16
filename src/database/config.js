e1f31de0c7a5ca11ff6c641a64934533aa0e914c
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-08-16 12:07:34 +0100

    chore(postgres):exports query function [Start  #167730727]

--- src/database/config.js
+++ src/database/config.js
@@ -1,12 +1,32 @@
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
+
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL
 });
 
 pool
   .connect()
   .then(() => console.log('connected to db'))
   .catch(err => console.error('connection error', err.stack));
+
+module.exports = {
+  /**
+   * DB Query
+   * @param {string} text
+   * @param {Array} params
+   * @returns {object} object
+   */
+  query(text, params) {
+    return new Promise((resolve, reject) => {
+      pool.query(text, params)
+        .then((res) => {
+          resolve(res);
+        }).catch((error) => {
+          reject(error);
+        });
+    });
+  },
+};
