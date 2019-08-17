1cee5266140dd1b13e2a00fc2db876f1ab890914
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-08-17 03:54:27 +0100

    chore(postgres):configure pg as db engine [Finish #167730727]

--- src/database/config.js
+++ src/database/config.js
@@ -7,26 +7,6 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
 
-pool
-  .connect()
+pool.connect()
   .then(() => console.log('connected to db'))
-  .catch(err => console.error('connection error', err.stack));
-
-module.exports = {
-  /**
-   * DB Query
-   * @param {string} text
-   * @param {Array} params
-   * @returns {object} object
-   */
-  query(text, params) {
-    return new Promise((resolve, reject) => {
-      pool.query(text, params)
-        .then((res) => {
-          resolve(res);
-        }).catch((error) => {
-          reject(error);
-        });
-    });
-  },
-};
+  .catch((err) => console.error('connection error', err.stack))
