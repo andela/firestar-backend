4b6d70c9a4f469d30f406c779733ec5bde118d66
Farce <a.rasheedalabi@gmail.com>
2019-08-20 18:11:19 +0100

    fix Hound issues

--- src/utils/hash/hash.js
+++ src/utils/hash/hash.js
@@ -2,8 +2,7 @@ import bcrypt from "bcryptjs";
 
 const hash = async password => {
   const salt = await bcrypt.genSaltSync(10);
-  const hash = await bcrypt.hashSync(password, salt);
-  return hash;
+  return bcrypt.hashSync(password, salt);
 };
 
 export default hash;
