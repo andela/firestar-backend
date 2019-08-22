d0c7983ec0a485793e17477c5499d166a8ea8e80
Zanio <akp.ani@yahoo.com>
2019-08-22 04:43:21 +0100

    chore(test):add test framework [Fix #167730740]

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,15 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const emailVerifyToken = id => new Promise((resolve, reject) => {
-	const token = jwt.sign({ id: id }, process.env.SECRET_KEY, { expiresIn: '1h' });
-	if (token) resolve(token);
-	if (!token) reject({ err: 'could not assign a token, make sure you provide a secret key' });
+const emailVerifyToken = (id) => new Promise((resolve, reject) => {
+  const token = jwt.sign({ id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+  if (token) resolve(token);
+  if (!token) reject({ err: 'could not assign a token, make sure you provide a secret key' });
 });
 
 const arrayTest = (array, index) => array.indexOf(index);
 
 
-
 export { emailVerifyToken, arrayTest };
-
