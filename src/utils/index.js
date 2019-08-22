c423e0dcef9756fa535e865c2211e4bd4b9c9a5f
Zanio <akp.ani@yahoo.com>
2019-08-22 11:13:56 +0100

    chorre(secretkey-jwt):debug jwt build error [Fix #167730740]

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const emailVerifyToken = (id) => new Promise((resolve, reject) => {
+  console.log(process.env.SECRET_KEY_EMAIL_VERIFY_TOKEN);
   const token = jwt.sign({ id }, process.env.SECRET_KEY_EMAIL_VERIFY_TOKEN, { expiresIn: '1h' });
   const error = { err: 'could not assign a token, make sure you provide a secret key' };
   if (token) resolve(token);
