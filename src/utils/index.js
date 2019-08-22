28775543bbf0ba79516dd5d7b7c323a2297cf189
Zanio <akp.ani@yahoo.com>
2019-08-22 09:55:41 +0100

    bug(hound-fix):fix hound warning error [Fix #167730740]

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,9 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const emailVerifyToken = (id) => new Promise((resolve, reject) => {
-  const token = jwt.sign({ id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+  const token = jwt.sign({ id }, process.env.SECRET_KEY_EMAIL_VERIFY_TOKEN, { expiresIn: '1h' });
+  const error = { err: 'could not assign a token, make sure you provide a secret key' };
   if (token) resolve(token);
-  if (!token) reject({ err: 'could not assign a token, make sure you provide a secret key' });
+  if (!token) reject(error);
 });
 
 const arrayTest = (array, index) => array.indexOf(index);
