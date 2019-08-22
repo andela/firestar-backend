49368dd35e8de60e4a6dc7791b0462af19f084a6
Zanio <akp.ani@yahoo.com>
2019-08-22 16:03:51 +0100

    chore(travis-fix):fix travis build fail [Start #167730740]

--- src/__tests__/mail.js
+++ src/__tests__/mail.js
@@ -10,25 +10,24 @@ chai.use(sinonChai);
 
 const { expect } = chai;
 
- describe('/EMAIL VERIFICATION ROUTE', () => {
-   it('It shoud return 200 success message', function (done) {
-    
-		this.timeout(20000);
-    
+describe('/EMAIL VERIFICATION ROUTE', () => {
+  it('It shoud return 200 success message', function (done) {
+    this.timeout(20000);
+
 
     const body = {
       email: 'akp.ani@yahoo.com',
       firstName: 'Aniefiok',
       lastName: 'Akpan'
     };
-     chai.request(app)
-       .post('/api/email-test')
-       .send(body)
-       .end((err, res) => {
+    chai.request(app)
+      .post('/api/email-test')
+      .send(body)
+      .end((err, res) => {
         expect(res.body.status).to.equal(200);
-        expect(res.body).to.be.a('object');      
+        expect(res.body).to.be.a('object');
         done();
-       });
-   });
- });
- 
+      });
+  });
+});
+
