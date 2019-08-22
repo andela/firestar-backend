9faf6d8da10d958e9ead2616e88a85009e58d8ad
Zanio <akp.ani@yahoo.com>
2019-08-22 11:31:45 +0100

    chore(dotenv-failure):fix dotenv failure [Fix #167730740]

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const emailVerifyToken = (id) => new Promise((resolve, reject) => {
-  console.log(process.env.SECRET_KEY_EMAIL_VERIFY_TOKEN);
   const token = jwt.sign({ id }, process.env.SECRET_KEY_EMAIL_VERIFY_TOKEN, { expiresIn: '24h' });
   const error = { err: 'could not assign a token, make sure you provide a secret key' };
   if (token) resolve(token);
