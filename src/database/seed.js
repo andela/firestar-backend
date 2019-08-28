b3eb5c30e987adf712e9f26cd074f32294240221
Farce <a.rasheedalabi@gmail.com>
2019-08-28 12:42:12 +0100

    includes check for test email and password in env [Finishes #167730744]

--- src/database/seed.js
+++ src/database/seed.js
@@ -1,31 +1,34 @@
 import models from '../models';
 import { sequelize } from '../models';
-
+console.log(process.env.SOME_PASSWORD);
+console.log(process.env.YOUR_EMAIL);
 const sync = async () => {
-  await models.User.create({
-    email: 'youremail2@andela.com',
-    role: 'passenger'
-  });
+  if (process.env.YOUR_EMAIL && process.env.SOME_PASSWORD) {
+    await models.User.create({
+      email: 'youremail2@andela.com',
+      role: 'passenger'
+    });
 
-  await models.User.create({
-    email: process.env.YOUR_EMAIL,
-    role: 'driver'
-  });
+    await models.User.create({
+      email: process.env.YOUR_EMAIL,
+      role: 'driver'
+    });
 
-  await models.Login.create({
-    email: 'youremail@andela.com',
-    password: 'password'
-  });
+    await models.Login.create({
+      email: 'youremail@andela.com',
+      password: 'password'
+    });
 
-  await models.Login.create({
-    email: process.env.YOUR_EMAIL,
-    password: process.env.SOME_PASSWORD
-  });
+    await models.Login.create({
+      email: process.env.YOUR_EMAIL,
+      password: process.env.SOME_PASSWORD
+    });
 
-  await models.Reset.create({
-    email: 'youremail@andela.com',
-    password: 'password'
-  });
+    await models.Reset.create({
+      email: 'youremail@andela.com',
+      password: 'password'
+    });
+  }
 };
 
 export default sequelize.sync({ force: true }).then(async () => {
