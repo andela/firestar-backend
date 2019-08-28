ea2454a2c0dd7fe6088630e7ff2ac578a7730fb0
Farce <a.rasheedalabi@gmail.com>
2019-08-28 11:28:52 +0100

    resolves Hound issues [Finishes #167730744]

--- src/models/index.js
+++ src/models/index.js
@@ -5,8 +5,7 @@ import Sequelize from 'sequelize';
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 
-let sequelize;
-sequelize = config.use_env_variable
+const sequelize = config.use_env_variable
   ? new Sequelize(process.env[config.use_env_variable], config)
   : new Sequelize(config.database, config.username, config.password, config);
 
