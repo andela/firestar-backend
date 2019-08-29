7d572ae3a76bdcf31e4968e8687f8e95382d0227
Zanio <akp.ani@yahoo.com>
2019-08-29 02:21:03 +0100

    chore(email-verification-validation):add validation to email verification [Fix #167730740]

--- src/middlewares/mail.js
+++ src/middlewares/mail.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import { verifyEmailTemplate } from '../services/mail/template/verifyEmail';
 import { emailVerifyToken } from '../utils/index';
+import { emailRegex } from '../validation/emailValidation';
 import Mail from '../services/mail/Mail';
 
 
@@ -43,6 +44,9 @@ export const SendVerificationEmail = async (req, res, next) => {
     if (response.message === 'No recipients defined') {
       return res.status(409).json({ status: 409, error: response.message });
     }
+    if (response.message === 'queryA ECONNREFUSED smtp.gmail.com') {
+      return res.status(409).json({ status: 409, error: response.message });
+    }
     if (response) {
       req.verificationMailResponse = response;
       req.emailToken = token;
@@ -50,5 +54,35 @@ export const SendVerificationEmail = async (req, res, next) => {
     }
   } catch (err) {
     return res.status(400).json({ status: 400, error: err });
+  }
+};
+/**
+ * @param {req} req contains the express object.
+ * @param {res} res contains the express response.
+ * @param {next} next calls the next middleware.
+ * @returns {next-middleware } returns the next middleware or throws an error.
+ * @returns {error} if any error occurs it throws an error.
+ */
+export const handleEmptyEmailBody = (req, res, next) => {
+  if (!req.body) {
+    res.status(403).json({ status: 403, error: 'No body property is presented in the req object' });
+  } else {
+    let { email, firstName, lastName } = req.body;
+    if (email && firstName && lastName) {
+      email = email.trim();
+      firstName = firstName.trim();
+      lastName = lastName.trim();
+      next();
+    } else {
+      res.status(403).json({ status: 403, error: 'Please fill in the required inputs' });
+    }
+  }
+};
+export const handleInvalidEmail = (req, res, next) => {
+  const emailCheck = emailRegex(req.body.email);
+  if (emailCheck) {
+    next();
+  } else {
+    res.status(403).json({ status: 403, error: 'Please provide a valid email' });
   }
 };
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { SendVerificationEmail } from '../../middlewares/mail';
+import { SendVerificationEmail, handleInvalidEmail, handleEmptyEmailBody } from '../../middlewares/mail';
 import emailverification from '../../controllers/emailController';
 
 const router = Router();
 
-router.post('/users/email/test', SendVerificationEmail, emailverification.signUp);
+router.post('/users/email/test', handleEmptyEmailBody, handleInvalidEmail,
+  SendVerificationEmail, emailverification.signUp);
 
 router.get('/users/email/verify', emailverification.confirmEmailVerificaionToken);
 
