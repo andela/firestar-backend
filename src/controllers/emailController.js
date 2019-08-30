fc851b300157a9fefbde6b896333d9276e50d265
Zanio <akp.ani@yahoo.com>
2019-08-30 10:45:31 +0100

    chore(spell-error):correct response grammer [Fix #167730740]

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -38,7 +38,7 @@ export default class emailVerificationController {
       jwt.verify(id, process.env.SECRET_KEY_EMAIL_VERIFY_TOKEN);
       return res.status(200).json({
         status: 200,
-        data: { message: 'You Account has been successfully verified, you would be redirected in few seconds to your dashboard' }
+        data: { message: 'Your Account has been successfully verified.' }
       });
     } catch (err) {
       return res.status(400).json({
