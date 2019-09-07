d892a484725a043ffe64c9b2a62cd75b4a217af0
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-09-07 11:57:27 +0100

    configure google and facebook strategies

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,4 +2,34 @@ import passport from 'passport';
 import FacebookStrategy from 'passport-facebook';
 import GoogleStrategy from 'passport-google-oauth20';
 
-passport.use('google', new GoogleStrategy)
+const {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_CALLBACK_URL,
+  FACEBOOK_CLIENT_ID,
+  FACEBOOK_CLIENT_SECRET,
+  FACEBOOK_CALLBACK_URL,
+} = process.env;
+
+
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: GOOGLE_CALLBACK_URL,
+  profileFields: ['name', 'photos', 'email']
+},
+(accessToken, refreshToken, profile, done) => done(null, profile)));
+
+passport.use(new FacebookStrategy({
+  clientID: FACEBOOK_CLIENT_ID,
+  clientSecret: FACEBOOK_CLIENT_SECRET,
+  callbackURL: FACEBOOK_CALLBACK_URL,
+  profileFields: ['name', 'photos', 'email']
+},
+(accessToken, refreshTocken, profile, done) => done(null, profile)));
+
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+export default passport;
