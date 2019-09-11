adca41639dba947e5e0fa977b42285cf12b7cd06
Zanio <akp.ani@yahoo.com>
2019-09-11 03:11:47 +0100

    chore(TTL-review):implement TTL review and consolidate mail package [Fix #167730738]

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ app.use(routes);
 
 // / catch 404 and forward to error handler
 app.use('*', (req, res) => {
-	res.status(404).json({ status: 404, err: 'That routes is not a known route' });
+  res.status(404).json({ status: 404, message: 'That routes is not a known route' });
 });
 
 // / error handlers
