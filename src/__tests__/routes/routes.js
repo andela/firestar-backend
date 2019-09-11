eedb5e2b0a608c9d06cb5ad33666d6ec53940a5d
Zanio <akp.ani@yahoo.com>
2019-09-11 03:19:28 +0100

    chore(TTL-review):implement TTL review and consolidate mail package [Fix #167730738]

--- src/__tests__/routes/routes.js
+++ src/__tests__/routes/routes.js
@@ -6,13 +6,13 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('Test for getting undefined routes', () => {
-  it('should return 200 for the default route', () => {
+  it('should return 404 for the default route', () => {
     chai
       .request(app)
       .post('/')
       .end((err, res) => {
         expect(res.status).to.equal(404);
-        expect(res.body.message).to.equal('Not Found');
+        expect(res.body.message).to.equal('That routes is not a known route');
       });
   });
 });
