9333a3025fb9993b56c18a73a3f44a7f0543bc41
Zanio <akp.ani@yahoo.com>
2019-09-11 18:22:43 +0100

    chore(add-migration):correct migration [Fix #167730738]

--- src/seeders/3-create-logins.js
+++ src/seeders/3-create-logins.js
@@ -13,12 +13,6 @@ module.exports = {
     password: 'password',
     createdAt: new Date(),
     updatedAt: new Date()
-  },
-  {
-    email: process.env.YOUR_EMAIL,
-    password: process.env.SOME_PASSWORD,
-    createdAt: new Date(),
-    updatedAt: new Date()
   }], {}),
 
   down: (queryInterface) => queryInterface.bulkDelete('logins', null, {})
